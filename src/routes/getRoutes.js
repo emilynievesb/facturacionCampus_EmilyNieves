b83e3a3e4b10c3d7c7d75caf728ba2603d4467a4
Emily <nemily1710@gmail.com>
2023-09-05 10:40:47 -0500

    feat: :sparkles: tokens
    Se realizan tokens de validación y limite

--- src/routes/getRoutes.js
+++ src/routes/getRoutes.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
 import { obtenerMedicamentosProveedoresController } from "../controllers/getController.js";
+import { limitPets, limitSize } from "../utils/limit.js";
+import {
+  authorizationMiddleware,
+  contentMiddlewareMedicamentos,
+} from "../utils/token.js";
 
 const getInitRoute = () => {
   const router = Router();
   //! 2.Listar los proveedores con su información de contacto en medicamentos
-  router.get("/obtenerProveedores", obtenerMedicamentosProveedoresController);
+  router.get(
+    "/obtenerProveedores",
+    limitPets,
+    limitSize,
+    //validación rol,
+    authorizationMiddleware,
+    contentMiddlewareMedicamentos,
+    obtenerMedicamentosProveedoresController
+  );
   return router;
 };
 
