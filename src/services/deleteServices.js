8e0c52b9e2866c8f52468f25567882707b96b3bb
Emily <nemily1710@gmail.com>
2023-09-05 13:30:55 -0500

    feat: :sparkles: Detalles finales
    Se realizo eliminación de archivo basura

--- src/services/deleteServices.js
+++
@@ -1,7 +0,0 @@
-import { Empleado } from "./postServices.js";
-
-const eliminarEmpleadoService = async (id) => {
-  const empleado = new Empleado();
-  return await empleado.eliminarEmpleado(id);
-};
-export { eliminarEmpleadoService };
