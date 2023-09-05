490dfecc686b970853b8d5d5950a0ef19e4a5649
Emily <nemily1710@gmail.com>
2023-09-05 13:29:47 -0500

    fix: :ambulance: Se corrigieron db
    Se corrigieron fechas en la db

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,27 +1,29 @@
 import { Router } from "express";
 // import { postInitRoute } from "./postRoutes.js";
-// import { getInitRoute } from "./getRoutes.js";
-// import { deleteRoute } from "./deleteRoutes.js";
-import { putInitRoute } from "./putRoutes.js";
+import { getInitRoute } from "./getRoutes.js";
+import { limitPets } from "../utils/limit.js";
 import { appToken } from "../utils/token.js";
+// import { deleteRoute } from "./deleteRoutes.js";
+// import { putInitRoute } from "./putRoutes.js";
 
 const v1Routes = () => {
   const router = Router();
-  //   router.use("/get", middlewareRateLimit, getInitRoute());
-  //   router.use("/post", middlewareRateLimit, postInitRoute());
+  router.use("/get", getInitRoute());
+  //   router.use("/post", postInitRoute());
   return router;
 };
 
 const v2Routes = () => {
   const router = Router();
-  router.use("/put", putInitRoute());
-  //   router.use("/delete", middlewareRateLimit, deleteRoute());
+  // router.use("/put", putInitRoute());
+  //   router.use("/delete", deleteRoute());
   return router;
 };
 
 const initRoute = () => {
   const router = Router();
-  router.use("/token", appToken);
+  router.use("/token", limitPets, appToken);
+  return router;
 };
 
 export { v1Routes, v2Routes, initRoute };
